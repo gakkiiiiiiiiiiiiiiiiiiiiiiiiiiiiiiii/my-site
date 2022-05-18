5d77517ecc0cd329de510402988295d94fd4d674
Allen <857119664@qq.com>
2022-05-18 15:45:04 +0800

    [A]add server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var exec = require('child_process').exec;
+var cmd = 'git pull && npm i && npm run build';
+
+http
+	.createServer(function (req, res) {
+		res.writeHead(200, { 'Content-Type': 'text/plain' });
+		res.end('hi');
+		exec(cmd, function (error, stdout, stderr) {
+			// 获取命令执行的输出
+			console.log({ error, stdout, stderr });
+		});
+	})
+	.listen(4567);
